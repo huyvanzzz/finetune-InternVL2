7d7454a54c52f8df47e899a3c2f5c44d3862d532
huyvanzzz <160515209+huyvanzzz@users.noreply.github.com>
2026-03-04 11:17:38 +0700

    Update train.py

--- train.py
+++ train.py
@@ -302,7 +302,7 @@ if __name__ == "__main__":
     if hasattr(model.language_model, "get_input_embeddings"):
         model.language_model.get_input_embeddings().to(torch.bfloat16)
 
-    hf_repo_id = "huyvanzzz/Internvl2.5-lora"
+    hf_repo_id = "huyvanzzz/Internvl2.5-lora_config"
     # peft_config = LoraConfig(
     #     r=config['model']['lora']['r'],
     #     lora_alpha=config['model']['lora']['alpha'],
